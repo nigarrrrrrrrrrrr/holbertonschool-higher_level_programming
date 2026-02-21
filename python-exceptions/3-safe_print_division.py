fd7ab4ac7c65100190b35f1cfe1312964921ea92
nigarrrrrrrrrrrr <nigarsuleymanova73@gmail.com>
2026-02-21 22:44:34 +0000

    Fix Task 3: result assignment and finally block

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = None
     try:
         result = a / b
     except (ZeroDivisionError, TypeError):
-        return None
+        result = None
     finally:
         print("Inside finally: {}".format(result))
     return result
