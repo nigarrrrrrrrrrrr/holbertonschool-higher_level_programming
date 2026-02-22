13c6e476037b89b02498796a93c8bccdd5262b68
nigarrrrrrrrrrrr <nigarsuleymanova73@gmail.com>
2026-02-22 12:58:31 +0000

    Fix all tasks: 0, 1, 2, and 3 with correct logic

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    nb = 0
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            nb += 1
+            count += 1
         except IndexError:
             break
     print("")
-    return nb
+    return count
