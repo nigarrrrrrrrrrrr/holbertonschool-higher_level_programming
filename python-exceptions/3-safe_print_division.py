2e41aee00250829758baecbcdae083234e26c437
nigarrrrrrrrrrrr <nigarsuleymanova73@gmail.com>
2026-02-22 16:27:06 +0000

    Fix Task 3: correct print message and logic

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
     except (ZeroDivisionError, TypeError):
         result = None
     finally:
-        print("Inside finally: {}".format(result))
+        print("Inside result: {}".format(result))
     return result
