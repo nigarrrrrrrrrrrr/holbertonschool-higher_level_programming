0f88991de7c321a96e6dfd424d5cda771e5dc6a6
nigar <nigarsuleymanova73@gmail.com>
2026-03-01 00:32:12 +0400

    Create Square class with size initializer
    Defines a Square class with an initializer for size.

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initializes the square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
