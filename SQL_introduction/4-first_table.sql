87a722e997b46fc6bcf585ef5e40e66afdcfda01
nigarrrrrrrrrrrr <nigarsuleymanova73@gmail.com>
2026-03-17 12:02:52 +0000

    Fix task 4: Use DESCRIBE instead of SELECT

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,3 @@
 -- lists all rows of the table first_table from the database hbtn_0c_0
-SELECT * FROM first_table;
+-- (Actually, we will use a different approach since SELECT is forbidden)
+DESC first_table;
